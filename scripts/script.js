b6642553a6cd16aed638690983b1fd675f119ecb
Justin Macharia Mwai <jmmwai@safaricom.co.ke>
2024-07-03 19:24:13 +0300

    initialize akan names

---
+++ scripts/script.js
@@ -0,0 +1,24 @@
+const maleAkanNames = {
+    0: "Kwasi",
+    1: "Kwadwo",
+    2: "Kwabena",
+    3: "Kwaku",
+    4: "Yaw",
+    5: "Kofi",
+    6: "Kwame"
+}
+
+const femaleAkanNames = {
+    0: "Akosua",
+    1: "Adwoa",
+    2: "Abenaa",
+    3: "Akua",
+    4: "Yaa",
+    5: "Afua",
+    6: "Ama"
+}
+
+const akanNames = {
+    male: maleAkanNames,
+    female: femaleAkanNames
+};
