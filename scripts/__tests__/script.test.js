c70778ad07b7f3167e2a42ce8af345d0abd86835
Justin Macharia Mwai <jmmwai@safaricom.co.ke>
2024-07-04 20:59:44 +0300

    fix failing tests

--- scripts/__tests__/script.test.js
+++ scripts/__tests__/script.test.js
@@ -1,21 +1,24 @@
+/**
+ * @jest-environment jsdom
+ */
 const getAkanName = require('./../script');
 
 test('Male akan name is returned correctly', () => {
-    expect(getAkanName("Male", "29/07/2024")).toBe("Kwadwo"); // monday
-    expect(getAkanName("Male", "30/07/2024")).toBe("Kwabena");
-    expect(getAkanName("Male", "31/07/2024")).toBe("Kwaku");
-    expect(getAkanName("Male", "31/12/2009")).toBe("Yaw");
-    expect(getAkanName("Male", "01/01/2010")).toBe("Kofi");
-    expect(getAkanName("Male", "01/04/1995")).toBe("Kwame");
-    expect(getAkanName("Male", "25/10/2015")).toBe("Kwasi"); // sunday
+    expect(getAkanName("Male", "2024-07-29")[1]).toBe("Kwadwo"); // monday
+    expect(getAkanName("Male", "2024-07-30")[1]).toBe("Kwabena");
+    expect(getAkanName("Male", "2024-07-31")[1]).toBe("Kwaku");
+    expect(getAkanName("Male", "2009-12-31")[1]).toBe("Yaw");
+    expect(getAkanName("Male", "2010-01-01")[1]).toBe("Kofi");
+    expect(getAkanName("Male", "1995-04-01")[1]).toBe("Kwame");
+    expect(getAkanName("Male", "2015-10-25")[1]).toBe("Kwasi"); // sunday
 });
 
 test('Female akan name is returned correctly', () => {
-    expect(getAkanName("Female", "01/03/1999")).toBe("Adwoa"); // monday
-    expect(getAkanName("Female", "29/02/2000")).toBe("Abenaa");
-    expect(getAkanName("Female", "01/03/2000")).toBe("Akua");
-    expect(getAkanName("Female", "18/07/2024")).toBe("Yaa");
-    expect(getAkanName("Female", "31/12/1999")).toBe("Afua");
-    expect(getAkanName("Female", "01/01/2000")).toBe("Ama");
-    expect(getAkanName("Female", "02/01/2000")).toBe("Akosua"); // sunday
+    expect(getAkanName("Female", "1999-03-01")[1]).toBe("Adwoa"); // monday
+    expect(getAkanName("Female", "2000-02-29")[1]).toBe("Abenaa");
+    expect(getAkanName("Female", "2000-03-01")[1]).toBe("Akua");
+    expect(getAkanName("Female", "2024-07-18")[1]).toBe("Yaa");
+    expect(getAkanName("Female", "1999-12-31")[1]).toBe("Afua");
+    expect(getAkanName("Female", "2000-01-01")[1]).toBe("Ama");
+    expect(getAkanName("Female", "2000-01-02")[1]).toBe("Akosua"); // sunday
 });
